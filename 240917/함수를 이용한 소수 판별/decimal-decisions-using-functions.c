c43df27a4077cd27551c254c4a54fff55061ffcb
yyseo1666 <156267051+yyseo1666@users.noreply.github.com>
2024-09-17 01:42:37 +0900

    Title: 함수를 이용한 소수 판별, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240917/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int Prime(int, int);
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", Prime(a, b));
+    return 0;
+}
+int Prime(int a, int b){
+    
+    int sum = 0;
+    for(int i = a; i<b+1; i++){
+        for(int j = 2; j<i; j++){
+            if(i%j){
+                if(j==i-1){
+                    sum+=i;
+                }
+            }
+            else{
+                break;
+            }
+        }
+    }
+    return sum;
+}
